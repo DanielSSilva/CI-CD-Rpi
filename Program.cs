1501e0f7d87e4d5f4cc4753cf839cedd1afff50b
Daniel Silva <daniel.sobral.silva.9@gmail.com>
2020-09-27 01:25:28 +0100

    trying to get cpu architecture

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CI_CD_tests_PI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"CPU architecture = {System.Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")}");
         }
     }
 }
