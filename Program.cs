a550d7690ef6f81748ce086f0d02adf21747f4a1
Daniel Silva <daniel.sobral.silva.9@gmail.com>
2020-09-27 01:00:35 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CI_CD_tests_PI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
