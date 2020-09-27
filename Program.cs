6f148fd8d490b5df2f44a62e8e156bcb3481a0b1
Daniel Silva <daniel.sobral.silva.9@gmail.com>
2020-09-27 22:25:26 +0100

    changed command

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace CI_CD_tests_PI
     class Program
     {
         static void Main(string[] args)
-        { 
+        {
             Console.WriteLine($"CPU architecture = {System.Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE")}");
         }
     }
